df3027efd5e0bc580878255252fd5b088c0c28c5
loicjovince <loicndikom20@gmail.com>
2024-12-09 23:41:17 +0100

    depot initié pour le site web

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='bg-success'>ggggggggggggggggggggggggggggggggggggggg</p>
+    </>
+  )
+}
+
+export default App
