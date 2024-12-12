1977be0e488864beedd73ec828dee1cf3f596719
vanellakenfack <you@example.comvanelkenfack7@gmail.com>
2024-12-12 12:12:05 +0100

    ajout image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,34 +4,33 @@ import Footer from './commun/Footer'
 
 function Home() {
   return (
-    <div>
-      <Header/>
-     <main>
-      
-     <section className="section-1">
-                 <div className="hero d-flex align-items-center">
-                    <div className="container-fluid">
-                        <div className="text-center">
-                            <span> welcome Amazing Constructions</span>
-                            <h1>Crafting dreams with <br /> precision and excellence .</h1>
-                            <p>we excel at transforming visions into reality through outstanding craftsmanship and precise <br /> attenton to detail . with years of experience and a dedication to quality </p>
-                               <div className="mt-3">
-                                  <button className="btn btn-primary">Contact Now</button> &
-                                  <button className="btn btn-secondary">View projets</button>
-                               </div>
+    <>
+    <Header/>
+   <main>
+    
+   <section className="section-1">
+               <div className="hero d-flex align-items-center">
+                  <div className="container-fluid">
+                      <div className="text-left">
+                          <span> welcome Amazing Constructions</span>
+                          <h1>Crafting dreams with <br /> precision and excellence .</h1>
+                          <p>we excel at transforming visions into reality through outstanding craftsmanship and precise <br /> attenton to detail . with years of experience and a dedication to quality </p>
+                             <div className="mt-3">
+                                <button className="btn btn-primary">Contact Now</button> &
+                                <button className="btn btn-secondary">View projets</button>
+                             </div>
 
-                        </div>
-                    </div>
-                 </div>
-              </section>
+                      </div>
+                  </div>
+               </div>
+            </section>
 
-     </main>
+   </main>
 
 
-      <Footer/>
-    </div>
+    <Footer/>
+  </>
   )
 }
 
 export default Home
-
