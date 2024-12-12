d58efd8aad2e2160aae3d63c93defbe849168479
vanellakenfack <you@example.comvanelkenfack7@gmail.com>
2024-12-12 09:33:18 +0100

    dossier image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import './assets/css/style.css'
-import Header from './components/commun/Header'
-import Footer from './components/commun/Footer'
 import Home from './components/Home'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
